6a32c8ca72ffccec9353ac84d6221a90e18b1ed1
philippashenkov <61788331+philippashenkov@users.noreply.github.com>
2023-02-02 14:26:04 -0500

    some refactoring

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require("express")
-const app = express()
- 
-const PORT = process.env.PORT || 3000
-app.use(express.json())
+const express = require("express");
+const app = express();
 
-const priceCalculation = (langType, mimetype="none"   , count) => {
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+const priceCalculation = (langType, mimetype = "none", count) => {
   let price = 0;
 
-  price +=
-  langType === "ru" || langType === "ua"
-      ? count * 0.05
-      : count * 0.12;
+  const languageMultiplier =
+    langType === "ru" || langType === "ua" ? 0.05 : 0.12;
+  price += count * languageMultiplier;
 
-  if (price < 50 && (langType === "ru" || langType === "ua"))
-    price = 50;
+  const minimumPrice =
+    langType === "ru" || langType === "ua" ? 50 : 120;
+  price = price < minimumPrice ? minimumPrice : price;
 
-  if (price < 120 && langType === "en") 
-  price = 120;
-
-  const expectedDocType = mimetype !=="doc" && mimetype !== "docx" && mimetype !== "rtf"
-  if (expectedDocType) 
-  price *= 1.2
+  const expectedDocTypes = ["doc", "docx", "rtf"];
+  if (!expectedDocTypes.includes(mimetype)) {
+    price *= 1.2;
+  }
 
   return price;
 };
@@ -30,53 +28,52 @@ const WEEKEND_HOURS = 0;
 
 const timeCalculation = (count, language) => {
   let workingHours = WORKING_HOURS;
-let symbolsPerHour = 333;
-if (language === 'ru' || language === 'ua') {
-symbolsPerHour = 1333;
-}
-workingHours = Math.ceil(count / symbolsPerHour);
+  let symbolsPerHour = 333;
+  if (language === "ru" || language === "ua") {
+    symbolsPerHour = 1333;
+  }
+  workingHours = Math.ceil(count / symbolsPerHour);
 
-let deadline = new Date();
-let workingDays = Math.ceil(workingHours / WORKING_HOURS);
+  let deadline = new Date();
+  let workingDays = Math.ceil(workingHours / WORKING_HOURS);
 
-while (workingDays > 0) {
-deadline.setDate(deadline.getDate() + 1);
-if (deadline.getDay() === 0 || deadline.getDay() === 6) {
-workingDays -= WEEKEND_HOURS;
-} else {
-workingDays -= 1;
-}
-}
-  const formattedDeadline = deadline.toLocaleDateString() + " " + deadline.toLocaleTimeString();
+  while (workingDays > 0) {
+    deadline.setDate(deadline.getDate() + 1);
+    if (deadline.getDay() === 0 || deadline.getDay() === 6) {
+      workingDays -= WEEKEND_HOURS;
+    } else {
+      workingDays -= 1;
+    }
+  }
+
+  const formattedDeadline =
+    deadline.toLocaleDateString() + " " + deadline.toLocaleTimeString();
   return formattedDeadline;
 };
 
-/*
+/*JSON body post request
 {
 	"language": "en",
 	"mimetype": "docx",
 	"count": 10000
 }
 */
-app.post("/neworder",  (req, res) => {
-  const newOrder = req.body;
-  const price = priceCalculation(
-    newOrder.language,
-    newOrder.mimetype,
-    newOrder.count
-  );
-  const deadline = timeCalculation(newOrder.count, newOrder.language);
-  res.json({price, deadline});
+
+app.post("/neworder", (req, res) => {
+  const { language, mimetype, count } = req.body;
+  const price = priceCalculation(language, mimetype, count);
+  const deadline = timeCalculation(count, language);
+  res.json({ price, deadline });
 });
 
 const start = async () => {
-	try {
-		app.listen(PORT, () => {
-			console.log(`Server has been started at port ${PORT}.\n`)
-		})
-	} catch (e) {
-		console.log(e)
-	}
-}
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server has been started at port ${PORT}.\n`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
